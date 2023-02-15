7cba7d4caa4331d0d3dd939a3773e31f35a87f91
Firesky57 <82162170+Firesky57@users.noreply.github.com>
2023-02-15 23:03:06 +0500

    3

---
+++ Pong/Assets/Paddle.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    float speed;
+    float Range;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 4.0f;
+        Range = 4.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Vertical");
+        float yOffset = move * Time.deltaTime * speed;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float moveY = Mathf.Clamp(rawYPos, -Range, Range);
+        transform.position = new Vector3(transform.localPosition.x, moveY, 0);
+
+
+    }
+}
